72142640136194ca35311a6f01b8c5a5f09ffc5e
Adam Dubey <adubey0311@gmail.com>
2022-03-29 09:58:31 -0400

    [AD][03M2922] Setup init client summaryForm and test

---
+++ client/src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+
+test('Init conditions', () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test('Checkbox enables button on first click and disables on second click', () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+
+  const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+  // first click
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  // second click
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
