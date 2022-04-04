10e7f38513fc64f3005fac6cae8e4a62bd30535d
Adam Dubey <adubey0311@gmail.com>
2022-04-04 08:23:59 -0400

    [AD][04M0422] Adding common alertBanner component for OrderEntry

--- client/src/pages/entry/Options.jsx
+++ client/src/pages/entry/Options.jsx
@@ -3,18 +3,24 @@ import { useEffect, useState } from 'react';
 import ScoopOption from './ScoopOption';
 import Row from 'react-bootstrap/Row';
 import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBanner';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        //
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
